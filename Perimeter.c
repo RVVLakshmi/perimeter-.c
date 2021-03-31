328790a5a630944fa75c82d2af823cbab37d1150
RVVLakshmi <81034948+RVVLakshmi@users.noreply.github.com>
2021-03-31 08:48:51 +0530

    Create perimeter.c

---
+++ Perimeter.c
@@ -0,0 +1,22 @@
+Write a c program to compute the perimeter and 
+Area of rectangle with a height of 7 inches and width 
+Width of 5 inches?
+
+#include<studio.h>
+Void main ()
+{
+int area, perimeter;
+Perimeter=2×(7+5);
+Printf("the perimeter value is %d\n", perimeter);
+Area=7×5;
+Printf("the area value is%d\n",area);
+}
+
+
+
+
+Output:
+enter the length of rectangle =7
+enter the breadth of rectangle=5
+the perimeter value is 24
+the area value is 35
